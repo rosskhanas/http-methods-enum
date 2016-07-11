e04bc27761d16cd3737d8c32f91c3fd1b7421ee9
Layton Whiteley <lwhiteley@users.noreply.github.com>
2016-07-11 12:03:19 -0400

    chore: add additional http methods
    add PATCH and HEAD method enums
    
    add PATCH and HEAD method enums - es6
    
    chore: add additional http methods

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const GET = 'GET';
 const POST = 'POST';
 const PUT = 'PUT';
 const DELETE = 'DELETE';
+const PATCH = 'PATCH';
+const HEAD = 'HEAD';
 
 export default {
   GET,
   POST,
   PUT,
   DELETE,
+  PATCH,
+  HEAD,
 };
